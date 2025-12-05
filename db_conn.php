e9eed885f8ed2c761667614ae247ffff55ee7aff
Water <water02120212@gmail.com>
2025-12-05 21:57:33 +0800

    線上資料庫
    請同步

--- db_conn.php
+++ db_conn.php
@@ -1,18 +1,17 @@
 <?php
-// 設定資料庫連線變數
-$servername = "localhost";
-$username = "root";
-$password = "water0212"; // XAMPP 預設為空，若有設定請自行修改
-$dbname = "Guild_Contribution_System";
+// 這裡要填 Alwaysdata 給你的資訊
+$servername = "mysql-water.alwaysdata.net"; 
+$username = "water_guild";     // 例如 guildteam_admin
+$password = "water0212";           // 例如 team1234
+$dbname = "water_guild_db";       // 例如 guildteam_guild_db
 
 // 建立連線
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// 檢查連線是否成功
+// 檢查連線
 if ($conn->connect_error) {
-    die("連線失敗 (Connection failed): " . $conn->connect_error);
+    die("連線失敗: " . $conn->connect_error);
 }
 
-// 設定編碼為 utf8mb4 (支援中文與 Emoji)
 $conn->set_charset("utf8mb4");
 ?>
